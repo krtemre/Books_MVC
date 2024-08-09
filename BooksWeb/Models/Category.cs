e8b9b639e5ca904789d3619787924c81e3d9f98a
krtemre <muhammedemrekurt@outlook.com>
2024-08-09 22:53:19 +0300

    Commit #2

---
+++ BooksWeb/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksWeb.Models
+{
+    public class Category
+    {
+        [Key] public int Id { get; set; } //Primary key
+        [Required] public string Name { get; set; } //Cannot be null in db (not null)
+        public int DisplayOrder { get; set; }
+    }
+}
