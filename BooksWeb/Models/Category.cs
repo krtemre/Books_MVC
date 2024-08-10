77347a8c0e27c02b27c4645bdcb949072f296f8b
krtemre <muhammedemrekurt@outlook.com>
2024-08-10 16:59:40 +0300

    Commit #3

--- BooksWeb/Models/Category.cs
+++ BooksWeb/Models/Category.cs
@@ -1,11 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BooksWeb.Models
 {
     public class Category
     {
         [Key] public int Id { get; set; } //Primary key
-        [Required] public string Name { get; set; } //Cannot be null in db (not null)
-        public int DisplayOrder { get; set; }
+
+        [Required] [MaxLength(30)] [DisplayName("Category Name")] public string Name { get; set; } //Cannot be null in db (not null)
+
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 - 100")] [DisplayName("Display Order")] public int DisplayOrder { get; set; }
     }
 }
