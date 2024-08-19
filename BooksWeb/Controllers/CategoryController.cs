bc1e00a087a968a0a417bd879c35beb0cf4bcc0a
krtemre <muhammedemrekurt@outlook.com>
2024-08-19 23:51:49 +0300

    Commit #4

--- BooksWeb/Controllers/CategoryController.cs
+++ BooksWeb/Controllers/CategoryController.cs
@@ -28,7 +28,7 @@ namespace BooksWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            if(obj.Name == obj.DisplayOrder.ToString())
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("name", "The Display Order cannot exactly same as Name.");
             }
@@ -36,10 +36,69 @@ namespace BooksWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfuly";
                 return RedirectToAction("Index");
             }
 
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound(); //Error view
+
+            var edit = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (edit == null)
+                return NotFound(); //Error view
+
+            return View(edit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly same as Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfuly";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound(); //Error view
+
+            var delete = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (delete == null)
+                return NotFound(); //Error view
+
+            return View(delete);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.FirstOrDefault(s => s.Id == id);
+
+            if (obj == null)
+                return NotFound();
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfuly";
+            return RedirectToAction("Index");
         }
     }
 }
